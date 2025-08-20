4380dc0ba310cb3ad860aa9dd41121a4066c4912
vladutmocanugabriel <mocanuvladutgabriel@gmail.com>
2025-08-20 17:44:58 +0300

    changed setup and toml

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='qa-project-gen',
     version='0.1.0',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
